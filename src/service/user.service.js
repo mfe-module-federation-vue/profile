ee0f7809353cbda17d5ad881bed96d0dc1c1020e
Alan <alanbschio@gmail.com>
2021-07-12 03:09:59 -0400

    faet: removing vuex not working after build

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,17 +1,18 @@
-import store from "root/store";
+// import store from "root/store";
 
 export const fetchUser = () => {
   return new Promise((resolve, reject) => {
-    if (store.getters.user) {
-      resolve(store.getters.user);
-    } else {
-      fetch("https://randomuser.me/api/")
-        .then((res) => res.json())
-        .then((user) => {
-          store.dispatch("setUser", user);
-          resolve(store.getters.user);
-        })
-        .catch(reject);
-    }
+    // if (store.getters.user) {
+    //   resolve(store.getters.user);
+    // } else {
+    fetch("https://randomuser.me/api/")
+      .then((res) => res.json())
+      .then((user) => {
+        // store.dispatch("setUser", user);
+        // resolve(store.getters.user);
+        resolve(user);
+      })
+      .catch(reject);
+    // }
   });
 };
